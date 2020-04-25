3743aaf8e5ea0fc80ca30357501fc531156492cb
steven.liatti <steven.liatti@hesge.ch>
2020-04-25 19:19:42 +0200

    Improve crawler, make it parallel

--- get_raw_data/crawler/src/main.rs
+++ get_raw_data/crawler/src/main.rs
@@ -1,8 +1,13 @@
+// TMDb movies crawler
+// Steven Liatti & Jeremy Favre
+
 use std::env;
 use std::fs::File;
 use std::io::{Write, BufReader, BufRead};
 use std::time::Instant;
 use serde::{Serialize, Deserialize};
+use std::sync::mpsc::channel;
+use std::thread;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Movie {
@@ -19,7 +24,7 @@ fn make_url(api_key: &String, id: usize) -> String {
 
 // Extract each id from the daily export file from TMDb API
 // and return an ids vector
-fn make_all_ids(input_file: &String) -> Vec<usize> {
+fn make_ids(input_file: &String) -> Vec<usize> {
     let file = File::open(input_file).unwrap();
     let reader = BufReader::new(file);
     let mut all_ids = vec![];
@@ -30,48 +35,69 @@ fn make_all_ids(input_file: &String) -> Vec<usize> {
     return all_ids;
 }
 
-// Extract from the main ids vector the ids requested by this machine
-// with the rule #ids / #machines approximatively. Jump to multiple of
-// #machines.
-fn make_this_ids(machines: usize, machine_id: usize, all_ids: &Vec<usize>) -> Vec<usize> {
-    let mut this_ids = vec![];
+// Divide ids vector in multiple sub vectors, one for each thread
+// Jump from multiple of threads to another to take current id
+fn make_thread_ids(machines: usize, machine_id: usize, all_ids: &Vec<usize>) -> Vec<usize> {
+    let mut thread_ids = vec![];
     let mut count: usize = machine_id;
     for (i, id) in all_ids.iter().enumerate() {
         if i == count {
-            this_ids.push(id.clone());
+            thread_ids.push(id.clone());
             count = count + machines;
         }
     }
-    return this_ids;
+    return thread_ids;
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start timer
     let now = Instant::now();
-    
+
+    // Args management
     let args: Vec<String> = env::args().collect();
     let api_key = &args[1];
     let input_file = &args[2];
     let output_file = &args[3];
-    let machines = if args.len() > 4 { args[4].parse().unwrap() } else { 1 };
-    let machine_id = if args.len() > 5 { args[5].parse().unwrap() } else { 1 };
-    
-    let all_ids = make_all_ids(input_file);
-    let ids = if args.len() > 5 { make_this_ids(machines, machine_id, &all_ids) } else { all_ids };
-    let mut movies = vec![];
-    
-    println!("Separate ids in {} seconds", now.elapsed().as_secs());
-    
-    for id in ids {
-        let movie = reqwest::get(&make_url(api_key, id)).await?.text().await?;
-        movies.push(movie);
+    let threads = &args[4].parse().unwrap();
+
+    // Extract all ids from TMDb daily export file
+    let ids = make_ids(input_file);
+
+    // Threads and channel management
+    let done = String::from("done");
+    let (crawlers, writer) = channel();
+
+    let handles = (0..*threads).into_iter().map(|i| {
+        let api_key_clone = api_key.clone();
+        let done_clone = done.clone();
+        let crawler = crawlers.clone();
+        // In each thread, make requests from own thread_ids and send it to writer
+        let thread_ids = make_thread_ids(*threads, i, &ids);
+        let handle = thread::spawn(move || {
+            for id in thread_ids {
+                let response = reqwest::blocking::get(&make_url(&api_key_clone, id)).unwrap().text().unwrap();
+                crawler.send(response).unwrap();
+            }
+            // When done, send "done" message
+            crawler.send(done_clone).unwrap();
+        });
+        handle
+    }).collect::<Vec<thread::JoinHandle<_>>>();
+
+    // Join all threads
+    for handle in handles {
+        handle.join().unwrap()
     }
-    println!("All requests in {} seconds", now.elapsed().as_secs());
-    
-    let mut output = File::create(output_file)?;
-    for movie in movies {
-        write!(output, "{}\n", movie)?;
+
+    // Write all responses in a file
+    let mut output = File::create(output_file).unwrap();
+    let mut counter = 0;
+    for message in writer {
+        if message == done { counter = counter + 1; }
+        else { write!( output, "{}\n", message).unwrap(); }
+        if &counter == threads { break; }
     }
-    println!("Write in {} seconds", now.elapsed().as_secs());
+
+    println!("Done in {} seconds", now.elapsed().as_secs());
     Ok(())
 }
