cf9447b73c7409d5ac4d6fce6c2fbab1989443a7
steven.liatti <steven.liatti@hesge.ch>
2020-06-01 14:48:07 +0200

    Update parser with algos

--- parser/src/main/scala/ch/hepia/Main.scala
+++ parser/src/main/scala/ch/hepia/Main.scala
@@ -40,10 +40,14 @@ object Main {
       AuthTokens.basic(loadConfig.database.username, loadConfig.database.password),
       config
     )
+
+    val algorithmService = new AlgorithmService(driver.asScala[Future])
     val movieService = new MovieService(driver.asScala[Future])
 
+    algorithmService.createConstraints()
     val movies = movieService.readMoviesFromFile("data/movies.json").toList
 
+    println("Data file from TMDb read, start first step")
     // First step : foreach movie, add it, with associated genres and peoples
     // and foreach people, link it to other peoples and genres
     movies.foreach(m => {
@@ -76,6 +80,8 @@ object Main {
       } yield if(p1.id != p2.id) movieService.addKnowsRelation(p1, p2)
     })
 
+    println("First step done, movies, genres and peoples added")
+
     // Second step : add similar and recommended movies
     // Add similar movies for each movie
     val similar = for {
@@ -89,8 +95,7 @@ object Main {
       m2 <- m1.recommendations.getOrElse(Recommendations(Nil)).results
     } yield movieService.addRecommendationsRelation(m1, m2)
 
-    // TODO: improve allPeople creation
-    // Last step : compute final people score
+    // Third step : compute final people score
     val allPeople = movies.flatMap(
       m => m.credits.cast.filter(a => a.order < actorsByMovie) :::
         m.credits.crew.filter(c => jobsForMovie.contains(c.job))
@@ -103,157 +108,19 @@ object Main {
 
     Await.result(fSimilar, Duration.Inf)
     Await.result(fRecommendations, Duration.Inf)
-    Await.result(fFinalPeopleScore, Duration.Inf)
+    println("Second step done, similar and recommended movies added")
 
-    Thread.sleep(3000) // TODO : 2546 noeuds 38435 relations
+    Await.result(fFinalPeopleScore, Duration.Inf)
+    println("Third step done, final peoples score computed")
+
+    Await.result(algorithmService.pagerank(), Duration.Inf)
+    Await.result(algorithmService.centrality(), Duration.Inf)
+    Await.result(algorithmService.genreDegree(), Duration.Inf)
+    Await.result(algorithmService.communities(), Duration.Inf)
+    Await.result(algorithmService.similarities(), Duration.Inf)
+    println("Fourth step done, compute some algorithms")
+
     driver.close()
+    println("End of main")
   }
 }
-
-// Requêtes utiles :
-/*
-Pour choper les relations d'un movie: MATCH p=()-[]->(m: Movie {id: 22})-[b: BELONGS_TO]->() RETURN p, b LIMIT 20
-Relation entre 2 people : MATCH (p2: People {name: 'William Lustig'})<-[r:KNOWS]-(p1: People {name: 'John Landis'}) RETURN p1, p2
-TODO: map entre people et liste de job/personnages pour avoir PLAY_IN / WORK_IN comme tableau
-TODO: améliorer division du score des movie makers
-
-
-// -----------------------------------------------------------------
-// CONTRAINTES
-TODO: créer contraintes (+ index gratuit) comme ceci
-CREATE CONSTRAINT ON (m:Movie) ASSERT m.id IS UNIQUE;
-CREATE CONSTRAINT ON (g:Genre) ASSERT g.id IS UNIQUE;
-CREATE CONSTRAINT ON (p:People) ASSERT p.id IS UNIQUE;
-
-// créer index
-CREATE INDEX ON :Movie(title);
-
-// -----------------------------------------------------------------
-// PAGERANK
-// créer graphe pour exécuter les algos
-
-// movies similar pagerank en mode "debug"
-// CALL gds.graph.create.cypher(
-//     'pagerank-movie-similar',
-//     'MATCH (m:Movie) RETURN id(m) AS id',
-//     'MATCH (a:Movie)-[:SIMILAR]->(b:Movie) RETURN id(a) AS source, id(b) AS target'
-// ) YIELD graphName, nodeCount, relationshipCount, createMillis;
-
-// movies similar pagerank en mode prod
-CALL gds.graph.create(
-    'pagerank-movie-similar',
-    'Movie',
-    'SIMILAR'
-);
-
-// execute pagerank on similar movies
-CALL gds.pageRank.write('pagerank-movie-similar', {
-  maxIterations: 20,
-  dampingFactor: 0.85,
-  writeProperty: 'pagerankSimilar'
-}) YIELD nodePropertiesWritten AS writtenProperties, ranIterations;
-
-
-// movies recommendations pagerank en mode prod
-CALL gds.graph.create(
-    'pagerank-movie-recommendations',
-    'Movie',
-    'RECOMMENDATIONS'
-);
-
-// execute pagerank on recommendations movies
-CALL gds.pageRank.write('pagerank-movie-recommendations', {
-  maxIterations: 20,
-  dampingFactor: 0.85,
-  writeProperty: 'pagerankRecommendations'
-}) YIELD nodePropertiesWritten AS writtenProperties, ranIterations;
-
-
-// show movies by pageRank recommendations
-// MATCH (m:Movie) 
-// RETURN DISTINCT m.title, m.pagerankRecommendations, m.pagerankSimilar
-// ORDER BY m.pagerankRecommendations DESC LIMIT 50;
-
-
-// -----------------------------------------------------------------
-// CENTRALITY
-
-// calcul le Degree Centrality pour les People avec KNOWS
-CALL gds.alpha.degree.write({
-  nodeProjection: 'People',
-  relationshipProjection: {
-    KNOWS: {
-      type: 'KNOWS',
-      projection: 'REVERSE'
-    }
-  },
-  writeProperty: 'knowsDegree'
-});
-
-// -----------------------------------------------------------------
-// GENRE DEGREE
-
-// ajout de propriétés de taille aux Genre
-MATCH (g:Genre)
-SET g.belongsToDegree = size((g)<-[:BELONGS_TO]-())
-SET g.knownForActingDegree = size((g)<-[:KNOWN_FOR_ACTING]-())
-SET g.knownForWorkingDegree = size((g)<-[:KNOWN_FOR_WORKING]-())
-SET g.knownForDegree = size((g)<-[:KNOWN_FOR_WORKING|:KNOWN_FOR_ACTING]-())
-SET g.degree = size((g)<-[:BELONGS_TO|:KNOWN_FOR_WORKING|:KNOWN_FOR_ACTING]-());
-
-
-// -----------------------------------------------------------------
-// COMMUNITY
-
-CALL gds.graph.create(
-    'people-knows-community-louvain-graph',
-    'People',
-    {
-        KNOWS: {
-            orientation: 'UNDIRECTED'
-        }
-    },
-    {
-        relationshipProperties: 'count'
-    }
-);
-
-CALL gds.louvain.write('people-knows-community-louvain-graph', { writeProperty: 'knowsCommunity' })
-YIELD communityCount, modularity, modularities;
-
-
-// -----------------------------------------------------------------
-// SIMILARITY MOVIES
-
-// create
-CALL gds.graph.create('movie-belongs-to-node-similar', ['Movie', 'Genre'], 'BELONGS_TO');
-
-// execute
-CALL gds.nodeSimilarity.write('movie-belongs-to-node-similar', {
-    writeRelationshipType: 'SIMILAR_MOVIES_ALGO',
-    writeProperty: 'score'
-}) YIELD nodesCompared, relationshipsWritten;
-
-// -----------------------------------------------------------------
-// SIMILARITY PEOPLES
-
-// create
-CALL gds.graph.create('people-known-for-acting-node-similar', ['People', 'Genre'], 'KNOWN_FOR_ACTING');
-
-// execute
-CALL gds.nodeSimilarity.write('people-known-for-acting-node-similar', {
-    writeRelationshipType: 'SIMILAR_FOR_ACTING',
-    writeProperty: 'score'
-}) YIELD nodesCompared, relationshipsWritten;
-
-
-// create
-CALL gds.graph.create('people-known-for-working-node-similar', ['People', 'Genre'], 'KNOWN_FOR_WORKING');
-
-// execute
-CALL gds.nodeSimilarity.write('people-known-for-working-node-similar', {
-    writeRelationshipType: 'SIMILAR_FOR_WORKING',
-    writeProperty: 'score'
-}) YIELD nodesCompared, relationshipsWritten;
-
- */
